1160bea38942cbc670b587a1a85198b00f55da7a
Park_Si_Yeong <111691870+phsria0825@users.noreply.github.com>
2026-04-01 17:44:35 +0900

    Title: 원소 10개의 합, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260401/원소 10개의 합/sum-of-10-elements.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int arr[10]; int sum = 0;
+
+    for(int i=0; i<size(arr); i++){
+        cin>>arr[i];
+        sum += arr[i];
+    }
+
+    cout<<sum<<endl;
+    return 0;
+}
