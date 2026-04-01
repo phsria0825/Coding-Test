e287ee3df00633657e324a915f6ef838854cbb07
Park_Si_Yeong <111691870+phsria0825@users.noreply.github.com>
2026-04-01 18:29:34 +0900

    Title: 배열 채우기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260401/배열 채우기/filling-array.cpp
+++ 260401/배열 채우기/filling-array.cpp
@@ -4,16 +4,18 @@ using namespace std;
 int main() {
     // Please write your code here.
     int n = 10;
+    vector<int> arr;
+    for(int i=0; i<n; i++){
+        int num;
+        cin>>num;
 
-    vector<int> arr(n);
-    for(int i=0; i<arr.size(); i++){
-        cin>>arr[i];
+        if(num == 0){
+            break;
+        }
+        arr.push_back(num);
     }
 
     for(int i=0; i<arr.size(); i++){
-        if(arr[arr.size()-1-i] == 0){
-            continue;
-        }
         cout<<arr[arr.size()-1-i]<<" ";
     }
     return 0;
