b7cd084f4375574919e9037892fda66427c262b4
Park_Si_Yeong <111691870+phsria0825@users.noreply.github.com>
2026-04-04 00:42:36 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260404/가장 왼쪽에 있는 최댓값/leftmost-max-value.cpp
+++ 260404/가장 왼쪽에 있는 최댓값/leftmost-max-value.cpp
@@ -5,27 +5,20 @@ using namespace std;
 int main() {
     int N;
     cin >> N;
-    int nums[N];
+    vector<int> nums(N);
     for (int i = 0; i < N; i++) {
         cin >> nums[i];
     }
 
-    // Please write your code here.
-    int idx = N;
-    vector<int> arr;
-    while(idx > 0){
-        int max_idx = 0;
-        for(int i=0; i<idx; i++){
-            if(nums[max_idx] < nums[i]){
-                max_idx = i;
-            }
-        }
-        arr.push_back(max_idx+1);
-        idx = max_idx;
-    }
 
-    for(int i=0; i<arr.size(); i++){
-        cout<<arr[i]<<" ";
+    // Please write your code here.
+    int left = N;
+    while(left > 0){
+        auto max_val = *max_element(nums.begin(), nums.begin() + left);
+        int idx = find(nums.begin(), nums.begin() + left, max_val) - nums.begin();
+
+        cout<<idx+1<<" ";
+        left = idx;
     }
 
     return 0;
