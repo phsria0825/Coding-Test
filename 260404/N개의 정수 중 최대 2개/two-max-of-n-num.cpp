1de2dc42cd80ce54d8fbc86479ae8912f8b5d625
Park_Si_Yeong <111691870+phsria0825@users.noreply.github.com>
2026-04-04 14:17:39 +0900

    Title: N개의 정수 중 최대 2개, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260404/N개의 정수 중 최대 2개/two-max-of-n-num.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin>>n;
+
+    vector<int> arr(n);
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    sort(arr.rbegin(), arr.rend());
+    cout<<arr[0]<< " " <<arr[1];
+    return 0;
+}
