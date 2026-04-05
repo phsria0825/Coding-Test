2c1c60b6fa776f8e74c32060bbe4e2ffcb2325fc
Park_Si_Yeong <111691870+phsria0825@users.noreply.github.com>
2026-04-05 10:11:39 +0900

    Title: 배열로 사각형 만들기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260405/배열로 사각형 만들기/print-array-in-rectangle.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    vector<vector<int>> arr(5, vector<int>(5));
+    for(int i=0; i<5; i++){
+        arr[i][0] = 1;
+        arr[0][i] = 1;
+    }
+
+    for(int i=1; i<5; i++){
+        for(int j=1; j<5; j++){
+            arr[i][j] = arr[i-1][j] + arr[i][j-1];
+        }
+    }
+
+    for(int i=0; i<5; i++){
+        for(int j=0; j<5; j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;;
+    }
+
+    return 0;
+}
