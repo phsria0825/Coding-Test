50d6229dd5ce4d2012317a61bf96a6d9ecc9ab6d
Park_Si_Yeong <111691870+phsria0825@users.noreply.github.com>
2026-04-20 11:15:06 +0900

    Title: 1이 3개 이상 있는 위치, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260420/1이 3개 이상 있는 위치/place-more-than-3-ones.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    cin>>N;
+
+    vector<vector<int>> grid(N, vector<int>(N));
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+            cin>>grid[i][j];
+        }
+    }
+
+    // Please write your code here.
+
+    int dr[4] = {-1,0,1,0};
+    int dc[4] = {0,1,0,-1};
+    int total_cnt = 0;
+
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+            int cnt = 0;
+
+            for(int k=0; k<size(dr); k++){
+                int nr = i + dr[k];
+                int nc = j + dc[k];
+
+                if(nr<0 || nr>=N || nc<0 || nc>=N) break;
+                if(grid[nr][nc] == 1){
+                    cnt++;
+                }
+
+                if(cnt>=3){
+                    total_cnt++;
+                }
+            }
+        }
+    }
+    cout<<total_cnt;
+    return 0;
+}
